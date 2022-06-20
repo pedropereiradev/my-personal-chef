1be6df94fbb4a796bc593b07b5599ec66e1508d7
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-20 18:23:45 -0300

    feat: redirect to correct routes implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
+  const history = useHistory();
+
   return (
     <Navbar data-testid="footer" fixed="bottom">
-      <button type="button">
+      <button type="button" onClick={ () => history.push('/drinks') }>
         <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
       </button>
-      <button type="button">
+      <button type="button" onClick={ () => history.push('/explore') }>
         <img src={ exploreIcon } alt="Explore Icon" data-testid="explore-bottom-btn" />
       </button>
-      <button type="button">
+      <button type="button" onClick={ () => history.push('/foods') }>
         <img src={ mealIcon } alt="Food Icon" data-testid="food-bottom-btn" />
       </button>
     </Navbar>
