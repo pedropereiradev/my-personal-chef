c58ac8919801c9cc2852b00cbe3105d745947147
Adriano Costa <adrianofacosta@gmail.com>
2022-06-20 19:24:10 -0300

    feat: add redirect link to profile page on header profile button

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ import searchTopBTN from '../images/searchIcon.svg';
 export default function Header() {
   const [title, setTitle] = useState('');
   const [hasSearchBtn, setHasSearchBtn] = useState(true);
-  const { location: { pathname } } = useHistory();
+  const history = useHistory();
+  const { location: { pathname } } = history;
 
   useEffect(() => {
     switch (pathname) {
@@ -54,7 +55,7 @@ export default function Header() {
     <header>
       <button
         type="button"
-        onClick={ () => console.log('teste profile') }
+        onClick={ () => history.push('/profile') }
       >
         <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
       </button>
