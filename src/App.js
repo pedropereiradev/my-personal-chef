74e2e1e7f3d04c2322de0964644239981235fb64
Geovana Augusta <geovanaag@hotmail.com>
2022-06-20 16:23:00 -0300

    fix: move BrowserRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './pages/Login';
 import Foods from './pages/Foods';
@@ -21,24 +21,26 @@ import FavoritesRecipes from './pages/FavoritesRecipes';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route path="/foods" component={ Foods } />
-      <Route path="/drinks" component={ Drinks } />
-      <Route path="/foods/:id" component={ DetailsFoods } />
-      <Route path="/drinks/:id" component={ DetailsDrinks } />
-      <Route path="/foods/:id/in-progress" component={ ProgressFoods } />
-      <Route path="/drinks/:id/in-progress" component={ ProgressDrinks } />
-      <Route path="/explore" component={ Explore } />
-      <Route path="/explore/foods" component={ ExploreFoods } />
-      <Route path="/explore/drinks" component={ ExploreDrinks } />
-      <Route path="/explore/foods/ingredients" component={ IngredientsFoods } />
-      <Route path="/explore/drinks/ingredients" component={ IngredientsDrinks } />
-      <Route path="/explore/foods/nationalities" component={ NationalitiesFoods } />
-      <Route path="/profile" component={ Profile } />
-      <Route path="/done-recipes" component={ DoneRecipes } />
-      <Route path="/favorite-recipes" component={ FavoritesRecipes } />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/foods" component={ Foods } />
+        <Route path="/drinks" component={ Drinks } />
+        <Route path="/foods/:id" component={ DetailsFoods } />
+        <Route path="/drinks/:id" component={ DetailsDrinks } />
+        <Route path="/foods/:id/in-progress" component={ ProgressFoods } />
+        <Route path="/drinks/:id/in-progress" component={ ProgressDrinks } />
+        <Route path="/explore" component={ Explore } />
+        <Route path="/explore/foods" component={ ExploreFoods } />
+        <Route path="/explore/drinks" component={ ExploreDrinks } />
+        <Route path="/explore/foods/ingredients" component={ IngredientsFoods } />
+        <Route path="/explore/drinks/ingredients" component={ IngredientsDrinks } />
+        <Route path="/explore/foods/nationalities" component={ NationalitiesFoods } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/done-recipes" component={ DoneRecipes } />
+        <Route path="/favorite-recipes" component={ FavoritesRecipes } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import BrowserRouter from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Provider from './context/Provider';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
 
