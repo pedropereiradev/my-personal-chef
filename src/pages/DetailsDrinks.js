e8f5f7d43ebcc9e3d3db400240eaba1f2caa0906
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-21 15:56:16 -0300

    feat: details page components implemented on Food and drink details page

--- src/pages/DetailsDrinks.js
+++ src/pages/DetailsDrinks.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import DetailsImage from '../components/DetailsImage';
+import DetailsIngredients from '../components/DetailsIngredients';
+import DetailsInstructions from '../components/DetailsInstructions';
+import DetailsRecommended from '../components/DetailsRecommended';
+import InitRecipeBtn from '../components/InitRecipeBtn';
 
 const DetailsDrinks = () => (
-  <div>DetailsDrinks</div>
+  <>
+    <DetailsImage />
+    <DetailsIngredients />
+    <DetailsInstructions />
+    <DetailsRecommended />
+    <InitRecipeBtn />
+  </>
 );
 
 export default DetailsDrinks;
--- src/pages/DetailsFoods.js
+++ src/pages/DetailsFoods.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import DetailsImage from '../components/DetailsImage';
+import DetailsIngredients from '../components/DetailsIngredients';
+import DetailsInstructions from '../components/DetailsInstructions';
+import DetailsRecommended from '../components/DetailsRecommended';
+import DetailsVideo from '../components/DetailsVideo';
+import InitRecipeBtn from '../components/InitRecipeBtn';
 
 const DetailsFoods = () => (
-  <div>DetailsFoods</div>
+  <>
+    <DetailsImage />
+    <DetailsIngredients />
+    <DetailsInstructions />
+    <DetailsVideo />
+    <DetailsRecommended />
+    <InitRecipeBtn />
+  </>
 );
 
 export default DetailsFoods;
