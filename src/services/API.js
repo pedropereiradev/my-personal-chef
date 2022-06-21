e7df6ca83904002fab9833c8b32a63dc5c0cccb4
Geovana Augusta <geovanaag@hotmail.com>
2022-06-21 17:09:43 -0300

    fix: workflows

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@ const fetchFood = async () => {
   try {
     const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
     const data = await response.json();
-    // console.log(data.meals);
+    //  console.log(data.meals);
     return data.meals;
   } catch (error) {
     console.error(error);
