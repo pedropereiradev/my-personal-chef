2d57317996a7f2717cebd8db751b563d30d67e3a
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-21 17:58:53 -0300

    feat: Detail page video implemented

--- src/components/DetailsVideo.jsx
+++ src/components/DetailsVideo.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 function DetailsVideo() {
+  const { recipeDetails } = useContext(Context);
+
+  const { strYoutube } = recipeDetails;
+
   return (
     <section>
       <h2>Video</h2>
       <iframe
-        src="https://www.youtube.com/watch?v=Ds1Jb8H5Sg8"
+        src={ `https://www.youtube.com/embed/${strYoutube && strYoutube.split('=')[1]}` }
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write;
         encrypted-media; gyroscope; picture-in-picture"
