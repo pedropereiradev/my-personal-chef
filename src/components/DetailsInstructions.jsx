3f342097e9ebb00da2295f8392c5a67f09e0098c
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-21 17:58:30 -0300

    feat: Detail page instructions implemented

--- src/components/DetailsInstructions.jsx
+++ src/components/DetailsInstructions.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
 
 function DetailsInstructions() {
+  const { recipeDetails } = useContext(Context);
+  const { strInstructions } = recipeDetails;
   return (
     <section>
       <h2>Instructions</h2>
       <p data-testid="instructions">
-        instrucoe spara fazer a receita
+        {strInstructions}
       </p>
     </section>
   );
