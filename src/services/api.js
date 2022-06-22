20487ac0fdca26308aac7f95632d183dacb9240c
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-21 21:22:41 -0300

    feat: request drink and meal by name implemented

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-const MEAL_DETAILS_BASE = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
-const DRINK_DETAILS_BASE = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+const MEAL_BASE = 'https://www.themealdb.com/api/json/v1/1/';
+const DRINK_BASE = 'https://www.thecocktaildb.com/api/json/v1/1/';
 
 export const requestMealDetails = async (id) => {
   try {
-    const response = await fetch(`${MEAL_DETAILS_BASE}${id}`);
+    const response = await fetch(`${MEAL_BASE}lookup.php?i=${id}`);
     const { meals } = await response.json();
     return meals;
   } catch (error) {
@@ -13,9 +13,29 @@ export const requestMealDetails = async (id) => {
 
 export const requestDrinkDetails = async (id) => {
   try {
-    const response = await fetch(`${DRINK_DETAILS_BASE}${id}`);
+    const response = await fetch(`${DRINK_BASE}lookup.php?i=${id}`);
     const { drinks } = await response.json();
     return drinks;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestDrink = async (name) => {
+  try {
+    const response = await fetch(`${MEAL_BASE}search.php?s=${name}`);
+    const { meals } = await response.json();
+    return meals;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const requestMeal = async (name) => {
+  try {
+    const response = await fetch(`${DRINK_BASE}search.php?s=${name}`);
+    const { meals } = await response.json();
+    return meals;
   } catch (error) {
     console.log(error);
   }
