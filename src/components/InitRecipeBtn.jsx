cf485e61021575f2ef316447556e4ab5661f300b
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-22 18:23:24 -0300

    feat: implemented doneRecipe verification to render start button

--- src/components/InitRecipeBtn.jsx
+++ src/components/InitRecipeBtn.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { DONE_RECIPES_TOKEN, readStorage } from '../services/recipesStorage';
 
 function InitRecipeBtn() {
   const history = useHistory();
   const location = useLocation();
+  const [enableStartBtn, setEnableStartBtn] = useState(true);
+
+  useEffect(() => {
+    const recipeId = location.pathname.split('/')[2];
+    const recipeType = location.pathname.split('/')[1];
+    const doneRecipes = readStorage(DONE_RECIPES_TOKEN);
+    const isDoneRecipe = doneRecipes
+      .some(({ id, type }) => (id === recipeId && recipeType.includes(type)));
+
+    setEnableStartBtn(!isDoneRecipe);
+  }, []);
 
   const handleClick = () => {
     history.push(`${location.pathname}/in-progress`);
   };
 
-  return (
+  return enableStartBtn && (
     <button
       type="button"
       className="fixed-bottom"
