7a7266ad828d50870c1dfdf3b0f2c0e2d8ae89a9
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-22 19:23:57 -0300

    fix: fix logic to favorite recipe

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,52 +1,40 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import { useLocation } from 'react-router-dom';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import BlackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import Context from '../context/Context';
-import { FAVORITE_RECIPES_TOKEN, SaveStorage } from '../services/recipesStorage';
+import {
+  FAVORITE_RECIPES_TOKEN,
+  readStorage, removeFromStorage, SaveStorage,
+} from '../services/recipesStorage';
 
 const copy = require('clipboard-copy');
 
 function DetailsImage() {
   const location = useLocation();
   const { recipeDetails } = useContext(Context);
-  const [favorite, setFavorite] = useState(whiteHeartIcon);
+  const [favorite, setFavorite] = useState(false);
   const [showMessage, setShowMessage] = useState(false);
+  const [recipeInfo, setRecipeInfo] = useState({
+    id: '',
+    type: '',
+    nationality: '',
+    category: '',
+    alcoholicOrNot: '',
+    name: '',
+    image: '',
+  });
 
   const handleFavoriteRecipe = () => {
-    setFavorite((prevFavorite) => (
-      prevFavorite === whiteHeartIcon ? BlackHeartIcon : whiteHeartIcon
-    ));
-
-    const foodPage = location.pathname.includes('foods');
-
-    let favoriteRecipe = {};
-
-    if (foodPage) {
-      favoriteRecipe = {
-        id: recipeDetails.idMeal,
-        type: 'food',
-        nationality: recipeDetails.strArea,
-        category: recipeDetails.strCategory,
-        alcoholicOrNot: '',
-        name: recipeDetails.strMeal,
-        image: recipeDetails.strMealThumb,
-      };
+    if (!favorite) {
+      SaveStorage(FAVORITE_RECIPES_TOKEN, recipeInfo);
     } else {
-      favoriteRecipe = {
-        id: recipeDetails.idDrink,
-        type: 'drink',
-        nationality: '',
-        category: '',
-        alcoholicOrNot: recipeDetails.strAlcoholic,
-        name: recipeDetails.strDrink,
-        image: recipeDetails.strDrinkThumb,
-      };
+      removeFromStorage(FAVORITE_RECIPES_TOKEN, recipeInfo.id);
     }
 
-    SaveStorage(FAVORITE_RECIPES_TOKEN, favoriteRecipe);
+    setFavorite((prevFavorite) => (!prevFavorite));
   };
 
   const handleShareRecipe = () => {
@@ -61,25 +49,56 @@ function DetailsImage() {
     }, MESSAGE_TIME);
   }
 
+  useEffect(() => {
+    const recipeId = location.pathname.split('/')[2];
+    const recipeType = location.pathname.split('/')[1];
+
+    const favoriteRecipes = readStorage(FAVORITE_RECIPES_TOKEN);
+
+    const isFavorite = favoriteRecipes
+      .some(({ id, type }) => (id === recipeId && recipeType.includes(type)));
+
+    if (recipeType === 'foods') {
+      setRecipeInfo({
+        id: recipeDetails.idMeal,
+        type: 'food',
+        nationality: recipeDetails.strArea,
+        category: recipeDetails.strCategory,
+        alcoholicOrNot: '',
+        name: recipeDetails.strMeal,
+        image: recipeDetails.strMealThumb,
+      });
+    } else {
+      setRecipeInfo({
+        id: recipeDetails.idDrink,
+        type: 'drink',
+        nationality: '',
+        category: recipeDetails.strCategory,
+        alcoholicOrNot: recipeDetails.strAlcoholic,
+        name: recipeDetails.strDrink,
+        image: recipeDetails.strDrinkThumb,
+      });
+    }
+
+    setFavorite(isFavorite);
+  }, []);
+
   return (
     <Card>
       <Card.Img
         variant="top"
-        src={ recipeDetails.strMealThumb
-          ? recipeDetails.strMealThumb : recipeDetails.strDrinkThumb }
+        src={ recipeInfo.image }
         data-testid="recipe-photo"
       />
       <Card.Body>
         <Card.Title data-testid="recipe-title">
-          {recipeDetails.strMeal
-            ? recipeDetails.strMeal : recipeDetails.strDrink}
+          { recipeInfo.name }
         </Card.Title>
         <Card.Subtitle
           className="mb-2 text-muted"
           data-testid="recipe-category"
         >
-          {recipeDetails.strMeal
-            ? recipeDetails.strCategory : recipeDetails.strAlcoholic}
+          { recipeInfo.type === 'food' ? recipeInfo.category : recipeInfo.alcoholicOrNot}
         </Card.Subtitle>
         <button
           type="button"
@@ -91,7 +110,11 @@ function DetailsImage() {
           type="button"
           onClick={ handleFavoriteRecipe }
         >
-          <img src={ favorite } alt="Favorite Icon" data-testid="favorite-btn" />
+          <img
+            src={ favorite ? BlackHeartIcon : whiteHeartIcon }
+            alt="Favorite Icon"
+            data-testid="favorite-btn"
+          />
         </button>
         {showMessage ? 'Link copied!' : ''}
       </Card.Body>
