d081eee5befa7ab22e0197a7694bc4f5cc9519ec
Geovana Augusta <geovanaag@hotmail.com>
2022-06-22 15:41:31 -0300

    fix: github error

--- src/services/API.js
+++ src/services/API.js
@@ -103,7 +103,7 @@ const fetchDrinkByCategory = async (category) => {
     const response = await
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
     const data = await response.json();
-    console.log(data.drinks);
+    // console.log(data.drinks);
     return data.drinks;
   } catch (error) {
     console.error(error);
