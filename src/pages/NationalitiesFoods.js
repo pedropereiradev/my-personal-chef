7a8ad99029fe10e0e17491962538027dcd2dc636
Geovana Augusta <geovanaag@hotmail.com>
2022-06-22 19:27:17 -0300

    feat: start notFound

--- src/pages/NationalitiesFoods.js
+++ src/pages/NationalitiesFoods.js
@@ -5,7 +5,7 @@ import Header from '../components/Header';
 const NationalitiesFoods = () => (
   <div>
     <Header />
-    NationalitiesFoods c
+    NationalitiesFoods aa
     <Footer />
   </div>
 );
