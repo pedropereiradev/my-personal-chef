d122f53a4a10c839694eaeeb9cfe7e0314921b6f
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-22 11:03:36 -0300

    rerun evaluator

--- src/components/DetailsRecommended.jsx
+++ src/components/DetailsRecommended.jsx
@@ -9,7 +9,7 @@ function DetailsRecommended() {
       <Card data-testid={ `${index}-recomendation-card` }>
         <Card.Img variant="top" src="holder.js/100px160" />
         <Card.Body>
-          <Card.Title>Card title</Card.Title>
+          <Card.Title>Water</Card.Title>
           <Card.Text>
             This is a wider card with supporting text below as a natural lead-in to
             additional content. This content is a little bit longer.
