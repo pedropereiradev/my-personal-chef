9b2f07a33ae225863903dc9964cb2021da727470
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-21 21:22:05 -0300

    feat: loading message implemented; refact: change API request method

--- src/pages/DetailsDrinks.js
+++ src/pages/DetailsDrinks.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import DetailsImage from '../components/DetailsImage';
 import DetailsIngredients from '../components/DetailsIngredients';
 import DetailsInstructions from '../components/DetailsInstructions';
 import DetailsRecommended from '../components/DetailsRecommended';
 import InitRecipeBtn from '../components/InitRecipeBtn';
+import Context from '../context/Context';
 
-const DetailsDrinks = () => (
-  <>
-    <DetailsImage />
-    <DetailsIngredients />
-    <DetailsInstructions />
-    <DetailsRecommended />
-    <InitRecipeBtn />
-  </>
-);
+const DetailsDrinks = () => {
+  const { getDetailsPageInfo, loading } = useContext(Context);
+  const location = useLocation();
+
+  useEffect(() => {
+    const endpoints = location.pathname.split('/');
+    getDetailsPageInfo(endpoints[1], endpoints[2]);
+  }, []);
+
+  return loading ? (
+    <h2>Carregando...</h2>
+  ) : (
+    <>
+      <DetailsImage />
+      <DetailsIngredients />
+      <DetailsInstructions />
+      <DetailsRecommended />
+      <InitRecipeBtn />
+    </>
+  );
+};
 
 export default DetailsDrinks;
--- src/pages/DetailsFoods.js
+++ src/pages/DetailsFoods.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import DetailsImage from '../components/DetailsImage';
 import DetailsIngredients from '../components/DetailsIngredients';
 import DetailsInstructions from '../components/DetailsInstructions';
 import DetailsRecommended from '../components/DetailsRecommended';
 import DetailsVideo from '../components/DetailsVideo';
 import InitRecipeBtn from '../components/InitRecipeBtn';
+import Context from '../context/Context';
 
-const DetailsFoods = () => (
-  <>
-    <DetailsImage />
-    <DetailsIngredients />
-    <DetailsInstructions />
-    <DetailsVideo />
-    <DetailsRecommended />
-    <InitRecipeBtn />
-  </>
-);
+const DetailsFoods = () => {
+  const { getDetailsPageInfo, loading } = useContext(Context);
+  const location = useLocation();
+
+  useEffect(() => {
+    const endpoints = location.pathname.split('/');
+    getDetailsPageInfo(endpoints[1], endpoints[2]);
+  }, []);
+
+  return loading ? <h2>Carregando...</h2> : (
+    <>
+      <DetailsImage />
+      <DetailsIngredients />
+      <DetailsInstructions />
+      <DetailsVideo />
+      <DetailsRecommended />
+      <InitRecipeBtn />
+    </>
+  );
+};
 
 export default DetailsFoods;
