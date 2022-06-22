733bf14a3a6a3b3300fddcf9856cac690be52e4b
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-22 18:02:28 -0300

    feat: recommended recipes carousel implemented

--- src/components/DetailsRecommended.jsx
+++ src/components/DetailsRecommended.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { CardGroup, Carousel } from 'react-bootstrap';
+import Context from '../context/Context';
+import CarouselCard from './CarouselCard';
 
 function DetailsRecommended() {
-  const index = 0;
+  const { recomendation } = useContext(Context);
+  console.log(recomendation);
   return (
-    <section>
+    <section className="mb-5">
       <h2>Recommended</h2>
-      <Card data-testid={ `${index}-recomendation-card` }>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Water</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Carousel>
+        <Carousel.Item>
+          <CardGroup className="d-flex justify-content-between">
+            <CarouselCard index="0" />
+            <CarouselCard index="1" />
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardGroup className="d-flex justify-content-between">
+            <CarouselCard index="2" />
+            <CarouselCard index="3" />
+          </CardGroup>
+        </Carousel.Item>
+        <Carousel.Item>
+          <CardGroup className="d-flex justify-content-between">
+            <CarouselCard index="4" />
+            <CarouselCard index="5" />
+          </CardGroup>
+        </Carousel.Item>
+      </Carousel>
     </section>
   );
 }
