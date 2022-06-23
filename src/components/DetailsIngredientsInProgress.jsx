81096e096dcc70a7cd0201263fb26b5442dd74c1
Adriano Costa <adrianofacosta@gmail.com>
2022-06-23 19:34:36 -0300

    feat: add redirect to "done recipe" path on click in "finish recipe" btn

--- src/components/DetailsIngredientsInProgress.jsx
+++ src/components/DetailsIngredientsInProgress.jsx
@@ -8,6 +8,8 @@ function DetailsIngredientsInProgress() {
     ingredientQuantity: [],
   });
 
+  const [checkIngredients, setCheckIngredients] = useState({});
+
   const { usedIngredients, ingredientQuantity } = ingredients;
 
   const setIngredientsInfo = (searchedStr) => {
@@ -20,6 +22,15 @@ function DetailsIngredientsInProgress() {
     return info;
   };
 
+  const handleChange = ({ target: { name, checked } }) => {
+    setCheckIngredients((prevCheckIngredients) => ({
+      ...prevCheckIngredients,
+      [name]: checked,
+    }));
+  };
+
+  console.log(ingredientQuantity);
+
   useEffect(() => {
     const usedIngredientsInfo = setIngredientsInfo('strIngredient');
     const ingredientQuantityInfo = setIngredientsInfo('strMeasure');
@@ -30,16 +41,20 @@ function DetailsIngredientsInProgress() {
     });
   }, [recipeDetails]);
 
+  console.log(checkIngredients);
+
   return (
     <section>
       <h2>Ingredients</h2>
       <ul>
         {usedIngredients.map((ingredient, index) => (
-          <li
-            key={ index }
-            data-testid={ `${index}-ingredient-step` }
-          >
-            <input type="checkbox" />
+          <li key={ index } data-testid={ `${index}-ingredient-step` }>
+            <input
+              type="checkbox"
+              name={ `${index}` }
+              onChange={ handleChange }
+              checked={ checkIngredients[`${index}`] }
+            />
             {ingredient}
             {' '}
             -
--- src/components/FinishRecipeBtn.jsx
+++ src/components/FinishRecipeBtn.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function FinishRecipeBtn() {
+  const history = useHistory();
   return (
-    <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+    <button
+      type="button"
+      data-testid="finish-recipe-btn"
+      onClick={ () => history.push('/done-recipes') }
+    >
+      Finish Recipe
+    </button>
   );
 }
