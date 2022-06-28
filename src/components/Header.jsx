914494d471ec163564b24ad69ae3febcde21013c
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-28 15:55:27 -0300

    refact: Header component Refactored

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
+import searchTopBTN from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
+
+export default function Header() {
+  const [title, setTitle] = useState('');
+  const [hasSearchBtn, setHasSearchBtn] = useState(false);
+  const [hasSearchBar, setHasSearchBar] = useState(false);
+  const history = useHistory();
+  const { location: { pathname } } = history;
+
+  useEffect(() => {
+    const titleNames = pathname.split(/-|\//gi);
+    const handleTitles = [];
+    titleNames.forEach((title1) => {
+      if (title1 !== '') {
+        handleTitles.push(title1.charAt(0).toUpperCase() + title1.slice(1));
+      }
+    });
+    if (handleTitles.length > 1) {
+      setTitle(`${handleTitles[0]} ${handleTitles[handleTitles.length - 1]}`);
+    } else {
+      setTitle(`${handleTitles[0]}`);
+    }
+
+    if (pathname === '/foods' || pathname === '/drinks') setHasSearchBtn(true);
+  }, [pathname]);
+
+  return (
+    <header>
+      <button
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
+        <img src={ profileIcon } alt="Profile Icon" data-testid="profile-top-btn" />
+      </button>
+      <h2 data-testid="page-title">{title}</h2>
+      { hasSearchBtn
+      && (
+        <button
+          type="button"
+          onClick={ () => setHasSearchBar(!hasSearchBar) }
+        >
+          <img src={ searchTopBTN } alt="Search Icon" data-testid="search-top-btn" />
+        </button>) }
+      { hasSearchBar && <SearchBar />}
+    </header>
+  );
+}
