db16bafe34a6cb6f7150c25fbd61f42e4e649af4
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-30 00:19:11 -0300

    refact: change button to Link and add two more options on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import { useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import favoriteIcon from '../images/favoriteIcon.svg';
+import doneIcon from '../images/doneIcon.svg';
 
 function Footer() {
-  const history = useHistory();
-
   return (
     <Navbar data-testid="footer" fixed="bottom">
-      <button type="button" onClick={ () => history.push('/drinks') }>
-        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
-      </button>
-      <button type="button" onClick={ () => history.push('/explore') }>
-        <img src={ exploreIcon } alt="Explore Icon" data-testid="explore-bottom-btn" />
-      </button>
-      <button type="button" onClick={ () => history.push('/foods') }>
-        <img src={ mealIcon } alt="Food Icon" data-testid="food-bottom-btn" />
-      </button>
+      <Container>
+        <Link to="/foods">
+          <img src={ mealIcon } alt="Food Icon" data-testid="food-bottom-btn" />
+        </Link>
+        <Link to="/drinks">
+          <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
+        </Link>
+        <Link to="/explore">
+          <img src={ exploreIcon } alt="Explore Icon" data-testid="explore-bottom-btn" />
+        </Link>
+        <Link to="/favorite-recipes">
+          <img src={ favoriteIcon } alt="Favorite Icon" />
+        </Link>
+        <Link to="/done-recipes">
+          <img src={ doneIcon } alt="Done Recipes Icon" />
+        </Link>
+      </Container>
     </Navbar>
   );
 }
