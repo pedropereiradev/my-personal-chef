b8598c36a31ef01603c3eda8bcba3ab152b180eb
Pedro Pereira <pedro.2108@hotmail.com>
2022-06-30 21:35:02 -0300

    style: Explore recipes page styled

--- src/pages/ExploreRecipes.jsx
+++ src/pages/ExploreRecipes.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
+import { Card, Container } from 'react-bootstrap';
 import Footer from '../components/Footer';
 import { fetchDrink, fetchFood } from '../services/API';
 import Header from '../components/Header';
-import ExploreButton from '../components/ExploreButton';
+
+import exploreByRecipe from '../images/exploreByRecipe.png';
+import exploreByNationalities from '../images/exploreByNationalities.png';
+import surpriseMe from '../images/surpriseMe.png';
 
 const ExploreRecipes = () => {
   const history = useHistory();
@@ -23,29 +27,46 @@ const ExploreRecipes = () => {
   };
 
   return (
-    <div>
+    <div className="bg-light">
       <Header />
-      <ExploreButton
-        testId="by-ingredient"
-        route={ `/explore/${pathname.includes('foods')
-          ? 'foods' : 'drinks'}/ingredients` }
-      >
-        By Ingredient
-      </ExploreButton>
-
-      {pathname.includes('foods') && (
-        <ExploreButton testId="by-nationality" route="/explore/foods/nationalities">
-          By Nationality
-        </ExploreButton>
-      )}
-
-      <button
-        type="button"
-        onClick={ () => randomRoute() }
-        data-testid="explore-surprise"
-      >
-        Surprise me!
-      </button>
+      <Container>
+        <Card className="mb-3">
+          <Link to="/explore/foods" data-testid="by-ingredient">
+            <Card.Img variant="top" src={ exploreByRecipe } />
+            <Card.Title
+              className="text-danger text-center my-3"
+            >
+              By Ingredient
+            </Card.Title>
+          </Link>
+        </Card>
+        {pathname.includes('foods') && (
+          <Card className="mb-3">
+            <Link to="/explore/foods" data-testid="by-nationality">
+              <Card.Img variant="top" src={ exploreByNationalities } />
+              <Card.Title
+                className="text-danger text-center my-3"
+              >
+                By Nationalities
+              </Card.Title>
+            </Link>
+          </Card>)}
+        <Card className="mb-3">
+          <button
+            type="button"
+            onClick={ () => randomRoute() }
+            data-testid="explore-surprise"
+            className="icon-button"
+          >
+            <Card.Img variant="top" src={ surpriseMe } />
+            <Card.Title
+              className="text-danger text-center my-3"
+            >
+              Surprise me
+            </Card.Title>
+          </button>
+        </Card>
+      </Container>
       <Footer />
     </div>
   );
