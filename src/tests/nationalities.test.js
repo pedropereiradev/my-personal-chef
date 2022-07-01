d6288fd81ac9a7674a8ebe99271a385890136d49
isabela <irf161295@gmail.com>
2022-07-01 08:47:15 -0300

    feat: localize filters / problem with options

--- src/tests/nationalities.test.js
+++ src/tests/nationalities.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
-import { nationalities, categories } from './helpers/dataNationalities';
+import { nationatilyFoods, categoriesList } from './helpers/dataNationalities';
 
 const NATIONALITIES = '/explore/foods/nationalities';
 const EXPLORE_FOODS = '/explore/foods';
@@ -21,8 +21,8 @@ describe('Tests Nationalities Foods', () => {
   test('Header and Footer must be render', () => {
     const { history } = renderWithRouter(<App />);
     history.push(NATIONALITIES);
-    const ingredientsTitle = screen.getByRole('heading');
-    expect(ingredientsTitle).toBeInTheDocument();
+    const nationalitiesTitle = screen.getByRole('heading');
+    expect(nationalitiesTitle).toBeInTheDocument();
     const foodsImg = screen.getByAltText(/Food Icon/i);
     expect(foodsImg).toBeInTheDocument();
     const drinksImg = screen.getByAltText(/Drink Icon/i);
@@ -31,12 +31,12 @@ describe('Tests Nationalities Foods', () => {
     expect(exploreImg).toBeInTheDocument();
   });
 
-  test('Verify filters are correctly render', () => {
+  test('Verify filters are correctly render', async () => {
     jest.spyOn(global, 'fetch').mockResolvedValue(
       {
         json: jest.fn()
-          .mockResolvedValue(nationalities)
-          .mockResolvedValueOnce(categories),
+          .mockResolvedValue(nationatilyFoods)
+          .mockResolvedValueOnce(categoriesList),
       },
     );
     const { history } = renderWithRouter(<App />);
@@ -47,7 +47,16 @@ describe('Tests Nationalities Foods', () => {
     });
     userEvent.click(nationalitiesButton);
     expect(history.location.pathname).toBe(NATIONALITIES);
-
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    const filters = await screen.findAllByRole('combobox');
+    expect(filters.length).toBe(2);
+    userEvent.selectOptions(
+      // Find the select element
+      await filters[0],
+      // Find and select the Ireland option
+      await screen.getByRole('option', { name: 'Italian' }),
+    );
+    expect(screen.getByRole('option', { name: 'Italian' }).selected).toBe(true);
     // identificar os filtros
 
     jest.restoreAllMocks();
